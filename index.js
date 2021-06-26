58e2431b981662bffb4bd1fe2cb726b4147df856
최한솔 <hansol416@naver.com>
2021-06-26 10:50:10 +0900

    DB Control Practice

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client } = require("@notionhq/client")
+require("dotenv").config();
+
+const notion = new Client({ auth: process.env.NOTION_TOKEN })
+const databaseId = process.env.NOTION_DATABASE_ID
+
+async function addItem(text) {
+  try {
+    await notion.request({
+      path: "pages",
+      method: "POST",
+      body: {
+        parent: { database_id: databaseId },
+        properties: {
+          title: { 
+            title:[
+              {
+                "text": {
+                  "content": text
+                }
+              }
+            ]
+          }
+        }
+      },
+    })
+    console.log("Success! Entry added.")
+  } catch (error) {
+    console.error(error.body)
+  }
+}
+
+addItem("Yurts in Big Sur, California")
